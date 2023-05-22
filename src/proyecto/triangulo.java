33b7e89ef599082617231811cc0397e29d859d3c
user <user@Examenmysql>
2023-05-22 11:42:16 +0200

    comit 1

---
+++ src/proyecto/triangulo.java
@@ -0,0 +1,14 @@
+package proyecto;
+
+public class triangulo {
+ 
+		  public static void main(String[] args) {  
+		    for(int j=0; j<=3; j++) {  
+		      for(int i=0; i<j; i++) {  
+		        System.out.print( "*" );  
+		      }  
+		      System.out.println();  
+		    }  
+		  }
+	}
+
