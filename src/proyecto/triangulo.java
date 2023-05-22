00b76af8ddc80b5baad0a3491af773648d0b23e4
user <user@Examenmysql>
2023-05-22 11:46:07 +0200

    commit 4

--- src/proyecto/triangulo.java
+++ src/proyecto/triangulo.java
@@ -3,7 +3,7 @@ package proyecto;
 public class triangulo {
  
 		  public static void main(String[] args) {  
-		    for(int j=0; j<=3; j++) {  
+		    for(int j=0; j<=5; j++) {  
 		      for(int i=0; i<j; i++) {  
 		        System.out.print( "*" );  
 		      }  
