220c5a786aa6c10e1f50c48f91ac5b548be99169
user <user@Examenmysql>
2023-05-24 10:56:04 +0200

    Commit3

---
+++ subir/src/subir/hola.java
@@ -0,0 +1,12 @@
+package subir;
+
+public class hola {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+		
+		System.out.println("Hola mundo!");
+	}
+
+}
